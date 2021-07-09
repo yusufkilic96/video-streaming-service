d895b1464375a6f9893910a92e91df4d0e0a775a
yusufkilic96 <yusufkilic96@hotmail.com>
2021-07-09 15:14:43 +0300

    First commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php require_once("includes/header.php"); ?>
+
+
+<?php
+if(isset($_SESSION["userLoggedIn"])) {
+    echo "user is logged in as " . $userLoggedInObj->getUsername();
+}
+else {
+    echo "not logged in";
+}
+?>
+
+
+<?php require_once("includes/footer.php"); ?>
+                
---
+++ watch.php
@@ -0,0 +1,34 @@
+<?php 
+require_once("includes/header.php"); 
+require_once("includes/classes/VideoPlayer.php");
+require_once("includes/classes/VideoInfoSection.php");
+
+if(!isset($_GET["id"])) {
+    echo "Nothing found";
+    exit();
+}
+
+$video = new Video($con, $_GET["id"], $userLoggedInObj);
+$video->incrementViews();
+?>
+
+<script src="assets/js/videoPlayerActions.js"></script>
+<div class="watchLeftColumn">
+
+<?php
+    $videoPlayer = new VideoPlayer($video);
+    echo $videoPlayer->create(true);
+
+    $videoPlayer = new VideoInfoSection($con, $video, $userLoggedInObj);
+    echo $videoPlayer->create();
+?>
+
+</div>
+
+<div class="suggestions">
+
+</div>
+
+
+<?php require_once("includes/footer.php"); ?>
+                
